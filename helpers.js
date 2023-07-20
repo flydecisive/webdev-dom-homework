fafa6b5fcafa2e5f062464d6df72eb0bf9ae7113
flydecisive <flydecisive@yandex.ru>
2023-07-20 11:43:13 +0300

    сделал новую функцию
    написал функцию для рендера комментариев
    изменил логику других функций
    создал объект, в котором хранятся данные о кометариях

--- helpers.js
+++ helpers.js
@@ -1,3 +1,22 @@
+// данные комментариев
+const comments = [
+  {
+    name: 'Глеб Фокин',
+    date: '12.02.22 12:18',
+    text: 'Это будет первый комментарий на этой странице',
+    likesCount: 3,
+    isLiked: false,
+  },
+  {
+    name: 'Варвара Н.',
+    date: '13.02.22 19:22',
+    text: 'Мне нравится как оформлена эта страница! ❤',
+    likesCount: 75,
+    isLiked: true,
+  },
+];
+
+// Получение даты комментария
 const getDate = () => {
   const months = [
     '01',
@@ -27,10 +46,52 @@ const getDate = () => {
   return result;
 };
 
+// рендер комментария
+const renderComments = (parent) => {
+  const commentsHtml = comments
+    .map((comment) => {
+      return `<li class="comment">
+          <div class="comment-header">
+            <div>${comment.name}</div>
+            <div>${comment.date}</div>
+          </div>
+          <div class="comment-body">
+            <div class="comment-text">
+              ${comment.text}
+            </div>
+          </div>
+          <div class="comment-footer">
+            <div class="likes">
+              <span class="likes-counter">${comment.likesCount}</span>
+              <button class="like-button ${
+                comment.isLiked ? '-active-like' : ''
+              }"></button>
+            </div>
+          </div>
+        </li>`;
+    })
+    .join('');
+
+  parent.innerHTML = commentsHtml;
+
+  initLikesEventListeners();
+};
+
+// добавление обработчика события для лайка
+const initLikesEventListeners = () => {
+  const commentElements = document.querySelectorAll('.comment');
+
+  commentElements.forEach((commentElement) => {
+    commentElement.addEventListener('click', (e) => {
+      console.log(e);
+    });
+  });
+};
+
+// Создание нового комментария
 const createComment = (formNameElement, formTextElement, event = null) => {
   const eventCode = event ? event.code : event;
   if (eventCode === 'Enter' || eventCode === null) {
-    const oldComments = commentsElement.innerHTML;
     let name = '';
     let comment = '';
 
@@ -53,25 +114,36 @@ const createComment = (formNameElement, formTextElement, event = null) => {
       name = formNameValue;
       comment = formTextValue;
 
-      commentsElement.innerHTML =
-        oldComments +
-        `<li class="comment">
-          <div class="comment-header">
-            <div>${name}</div>
-            <div>${getDate()}</div>
-          </div>
-          <div class="comment-body">
-            <div class="comment-text">
-              ${comment}
-            </div>
-          </div>
-          <div class="comment-footer">
-            <div class="likes">
-              <span class="likes-counter">0</span>
-              <button class="like-button"></button>
-            </div>
-          </div>
-        </li>`;
+      comments.push({
+        name: name,
+        date: getDate(),
+        text: comment,
+        likesCount: 0,
+        isLiked: false,
+      });
     }
   }
 };
+
+// Смена лайка при нажатии
+// const toggleLike = (elements) => {
+//   const likes = [];
+//   elements.forEach((element) => {
+//     likes.push(element.querySelector('.likes'));
+//   });
+//   likes.forEach((like) => {
+//     like.addEventListener('click', () => {
+//       const likesCounter = like.querySelector('.likes-counter');
+//       const likeButton = like.querySelector('.like-button');
+//       const activeLike = likeButton.classList.contains('-active-like');
+
+//       if (activeLike) {
+//         likeButton.classList.remove('-active-like');
+//         likesCounter.textContent = Number(likesCounter.textContent) - 1;
+//       } else {
+//         likeButton.classList.add('-active-like');
+//         likesCounter.textContent = Number(likesCounter.textContent) + 1;
+//       }
+//     });
+//   });
+// };
