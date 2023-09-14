a724107e0fe4540efdac6b60d203e9cec278a1e6
flydecisive <flydecisive@yandex.ru>
2023-09-14 10:26:22 +0300

    Добавляет слушатели события на форму

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-// import { getComments } from "./modules/api.js";
-// import { createComment } from "./modules/api.js";
-// import { comments } from "./modules/comments.js";
-
-// const button = document.querySelector(".add-form-button");
-// const formElement = document.querySelector(".add-form");
-// const formTextElement = formElement.querySelector(".add-form-text");
-// const formNameElement = formElement.querySelector(".add-form-name");
-
-// getComments(comments);
-
-// formElement.addEventListener("keyup", (event) => {
-//   if (event.code === "Enter") {
-//     createComment(formNameElement, formTextElement, event);
-//     getComments(comments);
-//   }
-// });
-
-// formElement.addEventListener("input", () => {
-//   if (
-//     (formTextElement.value !== "" && !formTextElement.value.match(/^\s*$/)) ||
-//     formNameElement.value !== ""
-//   ) {
-//     button.classList.remove("disabled");
-//     button.removeAttribute("disabled");
-//   }
-// });
-
-// button.addEventListener("click", () => {
-//   createComment(formNameElement, formTextElement);
-// });
 import { renderMainPage } from "./modules/renderMainPage.js";
 import { getComments } from "./modules/api.js";
 import { renderComments } from "./modules/renderComments.js";
@@ -44,7 +13,57 @@ function renderApp() {
   const rootEl = document.querySelector(".container");
   rootEl.innerHTML = renderMainPage(comments);
 
-  getComments().then((responseData) => {
-    renderComments(responseData.comments);
+  getComments()
+    .then((responseData) => {
+      renderComments(responseData.comments);
+    })
+    .catch((error) => {
+      if (error.message === "Failed to fetch") {
+        alert("Кажется, у вас сломался интернет, попробуйте позже");
+      }
+    })
+    .finally(() => {
+      initButtonEventListener();
+      initInputEventListener();
+      initEnterEventListener();
+    });
+}
+
+function initButtonEventListener() {
+  const button = document.querySelector(".add-form-button");
+
+  button.addEventListener("click", () => {
+    // createComment(formNameElement, formTextElement);
+    console.log("click submit");
+  });
+}
+
+function initInputEventListener() {
+  const formElement = document.querySelector(".add-form");
+  const formTextElement = document.querySelector(".add-form-text");
+  const formNameElement = document.querySelector(".add-form-name");
+  const button = document.querySelector(".add-form-button");
+
+  formElement.addEventListener("input", (event) => {
+    console.log(event.target.value);
+    if (
+      (formTextElement.value !== "" && !formTextElement.value.match(/^\s*$/)) ||
+      formNameElement.value !== ""
+    ) {
+      button.classList.remove("disabled");
+      button.removeAttribute("disabled");
+    }
+  });
+}
+
+function initEnterEventListener() {
+  const formElement = document.querySelector(".add-form");
+
+  formElement.addEventListener("keyup", (event) => {
+    if (event.code === "Enter") {
+      // createComment(formNameElement, formTextElement, event);
+      // getComments(comments);
+      console.log("enter");
+    }
   });
 }
